ffd0bf4eef920d2eeff53479eed2529ba50b09bc
ishanajmeri <ajmeriishansurat@gmail.com>
2020-07-13 12:07:46 +0530

    signin,signup,signout

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 var config = {
   apiKey: process.env.REACT_APP_KEY,
@@ -14,6 +15,22 @@ var config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  };
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  dopasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
