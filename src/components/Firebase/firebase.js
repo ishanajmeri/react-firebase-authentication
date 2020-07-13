40dc3856d7f921d8afeddb18483106cf709046f6
ishanajmeri <ajmeriishansurat@gmail.com>
2020-07-13 09:32:45 +0530

    created firebase config

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app';
+
+var config = {
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STROAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+export default Firebase;
